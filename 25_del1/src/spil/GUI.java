52cf51a0bfb3010b63f02e57ef4349cf56157206
Nicolas Vahman <kavehv@live.dk>
2017-10-10 15:57:40 +0200

    HOPEFULLY VIRKER DET

--- 25_del1/src/spil/GUI.java
+++ 25_del1/src/spil/GUI.java
@@ -40,11 +40,11 @@ public class GUI {
 	public void frame() {
 		
 		f = new JFrame("Terningespil");
-		f.setVisible(true);
+		
 		f.setSize(900, 500);
 		f.getBackground();
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	
+		f.setLocationRelativeTo(null);
 
 		p.setBackground(new Color(1,50,32));
 		kast = new JButton("KAST MED MIG");
@@ -68,7 +68,7 @@ public class GUI {
 		imageIcon2 = new ImageIcon("");
 		imageLabel1 = new JLabel(imageIcon1, JLabel.CENTER);
 		imageLabel2 = new JLabel(imageIcon2, JLabel.CENTER);
-		
+		f.setVisible(true); //
 		
 		
 		//Sætter placering af objekterne
