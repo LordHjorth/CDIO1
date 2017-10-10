0fa1d4018ceb25721046f200de16cd8d7424924f
UltimateDadBod <32635311+UltimateDadBod@users.noreply.github.com>
2017-10-10 21:16:56 +0200

    Du

--- 25_del1/src/spil/Terningespil.java
+++ 25_del1/src/spil/Terningespil.java
@@ -1,22 +1,75 @@
+
 package spil;
 
+import spil.Terning;
+
+import java.awt.Color;
+
+import javax.swing.*;
+
+import desktop_codebehind.Car;
+import desktop_fields.*;
 import desktop_resources.GUI;
+
+//import java.util.*;
+//import java.lang.*;
 
 public class Terningespil {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+    	Field[] fields=new Field[1];
+    	fields[0]=new Empty.Builder().build();
+    	GUI.create(fields);
+        Terning terning = new Terning(0, 0);
+        String spiller1,spiller2;
+        spiller1=JOptionPane.showInputDialog("Indtast navn, Spiller 1");
+        spiller2=JOptionPane.showInputDialog("Indtast navn, Spiller 2");
+        Car car1=new Car.Builder().primaryColor(Color.GREEN).secondaryColor(Color.GREEN).typeTractor().patternCheckered().build();
+        Car car2=new Car.Builder().primaryColor(Color.GREEN).secondaryColor(Color.GREEN).typeUfo().patternCheckered().build();
+        GUI.addPlayer(spiller1, 0,car1);
+        GUI.addPlayer(spiller2, 0,car2);
+        GUI.removeCar(1,spiller1);
+        GUI.removeCar(1,spiller2);
+        GUI.setChanceCard("Den episke kamp "+spiller1+" mod "+spiller2);
+        
+        
+        int runde = 1;
 
-		Terning terning = new Terning(0, 0);
+        do {
 
+            if (runde % 2 == 0) { //Hvis runde modulus 2 er lig 0, så er det spiller 2s tur, 
+            	GUI.getUserButtonPressed("Din tur "+ spiller2+ " - tryk på knappen for at kaste terningerne", "Kast terningerne");
+                terning.random(); // kaster terningerne
+                GUI.setDice(terning.getTerning1(), terning.getTerning2());
+                
+                GUI.setBalance(spiller2, terning.setPoint2(terning.getSum()));
+                
+                
 
-		
-		for (int i = 0; i < 40; i++) {
-			terning.random();
-			GUI.getUserButtonPressed("Tryk på 'KAST' for at kaste med terningerne", "KAST");
-			GUI.setDice(terning.getTerning1(), terning.getTerning2());
+            }
 
-		}
+            if (runde % 2 != 0) {
 
-	}
+                GUI.getUserButtonPressed("Din tur " + spiller1+ " - tryk på knappen for at kaste terningerne", "Kast terningerne");
+                terning.random(); // kaster terningerne
+                GUI.setDice(terning.getTerning1(), terning.getTerning2());
+
+                
+                GUI.setBalance(spiller1, terning.setPoint1(terning.getSum()));
+                
+                
+            } else {
+
+            }
+            runde++;
+        } while ((terning.getPoint1() < 40) && (terning.getPoint2() < 40));
+
+        if (terning.getPoint1() > terning.getPoint2()) {
+            GUI.showMessage("TILLYKKEEEEEEEE TIL "+spiller1.toUpperCase());
+        }
+        if (terning.getPoint2() > terning.getPoint1()) {
+        	GUI.showMessage("TILLYKKEEEEEEEE TIL "+spiller2.toUpperCase());
+        }
+    }
 
 }
